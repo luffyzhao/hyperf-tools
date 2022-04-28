4d9591bffc781d3f1a412eb465511761100acf78
luffyzhao <luffyzhao@vip.126.com>
2022-04-28 12:15:55 +0800

    Services

--- src/Command/Generator/SearchCommand.php
+++ src/Command/Generator/SearchCommand.php
@@ -22,4 +22,11 @@ class SearchCommand extends GeneratorCommand
     {
         return 'App\Search';
     }
+
+    /**
+     * @return string
+     */
+    protected function getNameInput(){
+        return trim($this->input->getArgument('name')) . "Search";
+    }
 }
