cc1d0396100d660bdda6c610cc3bb8f8e8e6b3ad
luffyzhao <luffyzhao@vip.126.com>
2022-05-05 17:54:30 +0800

    添加captcha

---
+++ publish/captcha.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'width' => 125,
+    'height' => 30,
+    'font' => null,
+    'fingerprint' => null,
+];
---
+++ src/Libraries/Captcha.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace LHyperfTools\Libraries;
+
+use Gregwar\Captcha\CaptchaBuilder;
+use Hyperf\Contract\ConfigInterface;
+use Psr\SimpleCache\CacheInterface;
+use Ramsey\Uuid\Uuid;
+
+class Captcha
+{
+    protected ConfigInterface $config;
+    protected CacheInterface $cache;
+
+    /**
+     * @param ConfigInterface $config
+     * @param CacheInterface $cache
+     */
+    public function __construct(ConfigInterface $config, CacheInterface $cache)
+    {
+        $this->config = $config;
+        $this->cache = $cache;
+    }
+
+    /**
+     * @return array
+     * @throws \Psr\SimpleCache\InvalidArgumentException
+     */
+    public function json(){
+        $captcha = CaptchaBuilder::create()->build($this->config->get('captcha.width'),$this->config->get('captcha.height'),$this->config->get('captcha.font'),$this->config->get('captcha.fingerprint'));
+        $key = Uuid::uuid1()->toString();
+        $this->cache->set($key, $captcha->getPhrase());
+        return [
+            'key' => $key,
+            'image' => $captcha->inline()
+        ];
+    }
+}
