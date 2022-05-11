0bdae621b6370402e25e6925bc4053091f894b2e
luffyzhao <luffyzhao@vip.126.com>
2022-05-11 15:56:44 +0800

    添加captcha

--- src/Command/MakeRepositoryCommand.php
+++ src/Command/MakeRepositoryCommand.php
@@ -1,77 +1,135 @@
 <?php
 
-namespace LHyperfTools\Command;
+namespace LHyperfTools\Command\Generator;
 
 use Hyperf\Command\Command;
+use Hyperf\Translation\FileLoader;
+use Hyperf\Utils\Filesystem\Filesystem;
 use Hyperf\Utils\Str;
 use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Finder\SplFileInfo;
 
-class MakeRepositoryCommand extends Command
+if (!defined('BASE_PATH')) {
+    define('BASE_PATH', __DIR__ . '/../../../../');
+}
+
+class ModuleCommand extends Command
 {
     public function __construct()
     {
-        parent::__construct("make:repository");
+        parent::__construct("gen:module");
     }
 
+    /**
+     * @throws \Exception
+     */
     public function handle()
     {
         $name = $this->input->getArgument('name');
-        if(empty($name)){
+        if (empty($name)) {
             throw new \InvalidArgumentException('name 参数不能为空！');
         }
+        $this->executeFiles('Model', $name);
 
-        $this->call('gen:repository', [
-            'name' => $name
-        ]);
-
-        if($this->makeServiceQuestion()){
-            $this->call('gen:service', [
-                'name' => $name
-            ]);
-        }
-
-
-        if($this->makeModelQuestion()){
-            $this->call('gen:model', [
-                'table' => Str::substr($name, strrpos($name, "\\") + 1)
-            ]);
-        }
+        $this->executeFiles('Request', $name);
     }
 
+
     /**
-     * @return false
+     * @param $module
+     * @param $name
+     * @return void
+     * @throws \Exception
      */
-    protected function makeServiceQuestion(): bool
+    private function executeFiles($module, $name)
     {
-        $question = new Question('是否对应生成Service?', 'yes/no');
-        $a = $this->output->askQuestion($question);
-        if (!in_array($a, ['yes', 'no'])) {
-            $this->output->warning('请输入 yes 或者 no !!');
-            $this->makeServiceQuestion();
-        }
-        if ($a === 'yes') {
-            return true;
-        }
-        return false;
+        $orPath = __DIR__ . '/stubs/' . Str::studly($module) . '/';
 
+        $files = $this->getFiles($orPath);
+        /** @var SplFileInfo $file */
+        foreach ($files as $file) {
+            $stub = $this->buildClass($file, $name);
+            $replacePath = __DIR__ . '/stubs/' . $module . '/';
+
+            $path = $this->buildPath($file, $orPath, $replacePath);
+
+            $this->makeDir($path);
+            file_put_contents($path . '/' . Str::studly($name) . '/' . $file->getBasename('.stub') . ".php", $stub);
+        }
     }
 
     /**
+     * @param SplFileInfo $file
+     * @param string $path
+     * @param string $replacePath
+     * @return string
+     */
+    private function buildPath(SplFileInfo $file, string $path, string $replacePath)
+    {
+        $orPath = $file->getPath();
+        return Str::replace($path, $replacePath, $orPath);
+    }
+
+    /**
+     *
+     * @param SplFileInfo $file
+     * @param string $name
+     * @return array|string
+     */
+    protected function buildClass(SplFileInfo $file, string $name): array|string
+    {
+        $stub = $file->getContents();
+        $stub = $this->replaceClass($stub, $name);
+        return $this->replaceDatabases($stub, $name);
+    }
+
+
+    /**
+     * @param string $path
+     * @return array
+     */
+    private function getFiles(string $path): array
+    {
+        $filesystem = new Filesystem();
+        return $filesystem->allFiles($path);
+    }
+
+    /**
+     * @param string $stub
+     * @param string $name
+     * @return string
+     */
+    private function replaceClass(string $stub, string $name): string
+    {
+        return str_replace('%MODULE%', Str::studly($name), $stub);
+    }
+
+    /**
+     * @param string $stub
+     * @param string $name
+     * @return string
+     */
+    private function replaceDatabases(string $stub, string $name)
+    {
+        return str_replace('%SMODULE%', Str::lower($name), $stub);
+    }
+
+
+    /**
+     * @param string $path
      * @return bool
+     * @throws \Exception
      */
-    protected function makeModelQuestion()
+    private function makeDir(string $path)
     {
-        $question = new Question('是否对应生成Model?', 'yes/no');
-        $a = $this->output->askQuestion($question);
-        if (!in_array($a, ['yes', 'no'])) {
-            $this->output->warning('请输入 yes 或者 no !!');
-            $this->makeModelQuestion();
+        if (!file_exists($path)) {
+            mkdir($path, 0755, true);
         }
-        if ($a === 'yes') {
-            return true;
+
+        if (!is_writable($path)) {
+            throw new \Exception(sprintf("%s 目录没有写入权限！", $path));
         }
-        return false;
+        return true;
     }
 
     /**
@@ -83,6 +141,4 @@ class MakeRepositoryCommand extends Command
             ['name', InputArgument::OPTIONAL, '这里是对这个参数的解释']
         ];
     }
-
-
 }
